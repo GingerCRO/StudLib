327153405d8c617e5416a630052f481c4b08114c
Filip Juračić <filipjuracic1@gmail.com>
2025-02-19 16:33:14 +0100

    Implemented algorithms to display student, book, and loan data, as well as to edit or delete existing loans

--- Forms/FormLoans.cs
+++ Forms/FormLoans.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using StudentLibrary.Classes;
 
@@ -9,6 +10,8 @@ namespace StudentLibrary
     {
 
         string dbConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C#\\StudLib\\Database\\Database.mdf;Integrated Security=True";
+        string queryDelete = "DELETE FROM Loans WHERE Id = @Id";
+        string queryLoad = "SELECT Loans.Id, Loans.StudentId, Students.Name, Students.Surname, Loans.BookId, Books.Title, Loans.LoanDate, Loans.NumberOfDays FROM Loans JOIN Students ON Loans.StudentId = Students.Id JOIN Books ON Loans.BookId = Books.Id";
 
         public FormLoans()
         {
@@ -17,15 +20,14 @@ namespace StudentLibrary
 
         private void FormLoans_Load(object sender, EventArgs e)
         {
-
-
-
+            DataRefresh();
         }
 
         private void DataRefresh()
         {
 
-            
+            lbLoans.Items.Clear();
+            LoadData();
 
         }
 
@@ -48,22 +50,101 @@ namespace StudentLibrary
         private void btnBorrow_Click(object sender, EventArgs e)
         {
 
-            
+            FormLoanDetails fld = new FormLoanDetails();
+
+            if (fld.ShowDialog() == DialogResult.OK)
+            {
+                DataRefresh();
+            }
 
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
 
-            
+            FormLoanDetails fld = new FormLoanDetails();
+
+            if (lbLoans.SelectedIndex < 0)
+            {
+                MessageBox.Show("You have to select a loan!");
+            }
+            else
+            {
+                fld.l = (Loan)lbLoans.SelectedItem;
+
+                if (fld.ShowDialog() == DialogResult.OK)
+                {
+                    DataRefresh();
+                }
+            }
 
         }
 
         private void btnReturn_Click(object sender, EventArgs e)
         {
 
-            
+            if (lbLoans.SelectedIndex < 0)
+            {
+                MessageBox.Show("You have to select a loan!");
+            }
+            else
+            {
+                using (SqlConnection connection = new SqlConnection(dbConnection))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(queryDelete, connection))
+                    {
 
+                        Loan loanToDelete = (Loan)lbLoans.SelectedItem;
+
+                        command.Parameters.AddWithValue("@Id", loanToDelete.Id);
+
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+                DataRefresh();
+            }
+
+        }
+
+        private void LoadData()
+        {
+
+            using (SqlConnection connection = new SqlConnection(dbConnection))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(queryLoad, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Loan loan = new Loan();
+                            loan.Id = reader.GetInt32(0).ToString();
+
+                            Student student = new Student();
+                            student.Id = reader.GetInt32(1).ToString();
+                            student.Name = reader.GetString(2);
+                            student.Surname = reader.GetString(3);
+
+                            Book book = new Book();
+                            book.Id = reader.GetInt32(4).ToString();
+                            book.Title = reader.GetString(5);
+
+                            loan.LoanDate = reader.GetDateTime(6);
+                            loan.NumberOfDays = reader.GetInt32(7);
+
+                            loan.Student = student;
+                            loan.Book = book;
+
+                            lbLoans.Items.Add(loan);
+
+                        }
+                    }
+                }
+            }             
         }
 
     }
